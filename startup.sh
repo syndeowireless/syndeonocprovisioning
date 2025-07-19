45e5353e0c759e4d18e0be21f22e4d2a569f5971
syndeowireless <noc@syndeowireless.com>
2025-07-19 13:21:47 -0300

    Update startup.sh
    Install node command

--- startup.sh
+++ startup.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+#CHECKS IF NODE IS INSTALLED, IF NOT - INSTALL NODE#
+if ! command -v node &> /dev/null; then
+    echo "Setting up Node.js..."
+    curl -fsSL https://deb.nodesource.com/setup_18.x | bash - 
+    apt-get install -y nodejs
+fi
+
+
+cd /home/site/wwwroot
+npm install --no-audit --progress=false
+npm run build  #BUILD FOR PRODUCTION, DEV FOR LOCAL#
+
+
+echo "Starting server..."
 php -S 0.0.0.0:8080 -t public
