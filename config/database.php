4158f666754e0828f6ed2c7f8946ce08f616a42c
OliveiraNortonGauss <c.oliveira@nortongauss.com>
2025-07-21 14:05:15 -0300

    database SSL ERROR

--- config/database.php
+++ config/database.php
@@ -59,7 +59,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('DB_SSL', false) ? base_path('certs/BaltimoreCyberTrustRoot.crt.pem') : null,
+                PDO::MYSQL_ATTR_SSL_CA => base_path('certs/BaltimoreCyberTrustRoot.crt.pem'),
             ]) : [],
         ],
 
