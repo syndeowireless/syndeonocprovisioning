f20799f447cfd909f701e954ae3bbced96be825a
OliveiraNortonGauss <c.oliveira@nortongauss.com>
2025-07-21 12:09:06 -0300

    error SSL Certs in the folder.

--- config/database.php
+++ config/database.php
@@ -59,7 +59,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('DB_SSL', false) ? base_path('certs/BaltimoreCyberTrustRoot.crt.pem') : null,
             ]) : [],
         ],
 
