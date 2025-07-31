f4954d450f99c0f77b347fec00bd9b5f4b7e5cfa
OliveiraNortonGauss <c.oliveira@nortongauss.com>
2025-07-31 17:00:38 -0300

    create.blade changes

--- resources/views/network-provisioning/create.blade.php
+++ resources/views/network-provisioning/create.blade.php
@@ -4,16 +4,6 @@
     </div>
 @endif
 
-@if($errors->any())
-    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-6">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-
 @extends("layouts.app")
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -140,6 +130,15 @@
                     </button>
                 </div>
             </form>
+            @if($errors->any())
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-6">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
         </div>
     </div>
 </div>
