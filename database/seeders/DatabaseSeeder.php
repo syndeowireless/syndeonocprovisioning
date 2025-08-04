ffe2887b26362dc675cf7be5c71734b45a4f71ea
OliveiraNortonGauss <c.oliveira@nortongauss.com>
2025-08-04 20:07:33 -0300

    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,5 +13,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(IpSeeder::class);
+
+        $this->call(xmlTemplateSeeder::class);
+
     }
+    
+
+
 }
