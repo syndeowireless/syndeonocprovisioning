007ba62eab01d7c5041aaf40e7c914ccdd3cf053
OliveiraNortonGauss <c.oliveira@nortongauss.com>
2025-08-06 14:21:29 -0300

    sidevar -backgroundcolor v3

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -16,6 +16,9 @@
     .sub-menu .mm-collapse .mm-show  ul{
         background-color: #13395d
     }
+    .has-arrow .waves-effect a{
+        background-color: #13395d
+    }
 </style>
 <!-- ========== Left Sidebar Start ========== -->
             <div class="vertical-menu" style="background: #13395d; border-right: 5px solid #fbbf0f;" collapsed>
