41e6c116949566d722ac0772ec948c76b9b8f733
Shovonx47 <shovon@lamptechs.com>
2025-08-22 16:13:06 +0600

    Button CSS changed

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-[#13395d] border-2 border-[#fbbf0f] rounded-lg font-medium text-sm text-white transition-all duration-200 ease-in-out mt-4 mb-4 min-w-[120px] submit-button focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2']) }}>
     {{ $slot }}
 </button>
