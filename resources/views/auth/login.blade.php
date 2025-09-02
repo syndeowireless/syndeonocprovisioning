8a928b49c8523cb0c1c4160c5b9b75bac232f825
FARDIN98 <fardinahmed66@gmail.com>
2025-09-02 12:43:06 +0600

    style(auth): update login button styling for better UI consistency
    Replace default button styling with custom class and larger size to match design guidelines

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,7 +34,10 @@
                 </div>
             </div>
             <div class="col-6 text-end">
-                <x-primary-button class="btn btn-primary w-md waves-effect waves-light">
+                <!-- <x-primary-button class="btn btn-primary w-md waves-effect waves-light">
+                    {{ __('Log in') }}
+                </x-primary-button> -->
+                <x-primary-button class="btn btn-custom btn-lg">
                     {{ __('Log in') }}
                 </x-primary-button>
             </div>
