5fbcefbf0322bd17b0dbe0968009e917e34d8bf4
FARDIN98 <fardinahmed66@gmail.com>
2025-09-04 22:29:04 +0600

    feat(profile): add manage users page layout
    Implement basic layout structure for user management page using Bootstrap container and card components

--- resources/views/profile/manage-users.blade.php
+++ resources/views/profile/manage-users.blade.php
@@ -1,1 +1,23 @@
-<div>Manage-users</div>
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box d-flex align-items-center justify-content-between">
+                <h4 class="mb-0">Manage Users</h4>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <p>User management functionality will be implemented here.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
