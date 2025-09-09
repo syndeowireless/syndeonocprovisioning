97b46aee17466abfdb5bb854a75d3a88b71417b6
FARDIN98 <fardinahmed66@gmail.com>
2025-09-10 00:51:38 +0600

    refactor(views): remove commented footer links from 403 error page

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -105,10 +105,3 @@
 </style>
 @endsection
 
-<!-- @section('footer-links')
-<div class="mt-4 text-center">
-    <p class="text-muted small">
-        &copy; {{ date('Y') }} Syndeo. All rights reserved.
-    </p>
-</div>
-@endsection -->
